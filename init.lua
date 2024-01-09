2d75db9dc68eb47f55ede22368f16746d4e011e9
clayjoseck <clayjoseck@yahoo.com>
2024-01-08 19:49:13 -0600

    Add trouble.nvim and keymap from the primeagen to move lines in visual mode

--- init.lua
+++ init.lua
@@ -322,6 +322,10 @@ vim.o.termguicolors = true
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Keymap from The Primeagen to move lines while in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
