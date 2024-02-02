b77f77804a658d6467bc24433db8dfcb7c86a5f3
clayjoseck <clayjoseck@yahoo.com>
2024-02-02 14:24:05 -0600

    Add neotree toggle keymap

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -5,18 +5,19 @@
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  version = "*",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function ()
-    require('neo-tree').setup {
-      window = {
-	position = "right"
-      }
-    }
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require('neo-tree').setup {
+			window = {
+				position = "right"
+			}
+		}
+		vim.keymap.set('n', '<leader>N', '<Cmd>Neotree toggle<CR>')
+	end,
 }
