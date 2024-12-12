a7041cadefa7bca2c8356d1fc51bbcf2b9e61569
Clayton Joseck <clayton.joseck@coxautoinc.com>
2024-12-12 13:47:55 -0600

    Add more keymaps and trouble-nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymap from The Primeagen to move lines while in visual mode
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Keymap from TJDevries in the Configure Neovim's Options video at 1:16
+-- for executing current line and sourcing file (for nv config)
+vim.keymap.set('n', '<space>-', '<cmd>source %<CR>')
+vim.keymap.set('n', '<space>*', ':.lua<CR>')
+vim.keymap.set('v', '<space>*', ':lua<CR>')
