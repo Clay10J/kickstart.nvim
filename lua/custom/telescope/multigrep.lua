05dcd4bd66e9bb9c2a98daf9e349cbacde4e3ba4
Clayton Joseck <clayton.joseck@coxautoinc.com>
2025-01-08 15:43:46 -0600

    Add ts/js language server and add hidden file/directory search to multigrep

--- lua/custom/telescope/multigrep.lua
+++ lua/custom/telescope/multigrep.lua
@@ -29,7 +29,10 @@ local live_multigrep = function(opts)
         table.insert(args, pieces[2])
       end
 
-      return vim.iter({ args, { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' } }):flatten():totable()
+      return vim
+        .iter({ args, { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '--hidden' } })
+        :flatten()
+        :totable()
     end,
     entry_maker = make_entry.gen_from_vimgrep(opts),
     cwd = opts.cwd,
